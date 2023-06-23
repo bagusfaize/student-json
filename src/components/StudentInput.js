01afab7ede89c92ea003abe0dd171d075e68fdcd
bagus faize <bagus.faize@gmail.com>
2023-06-23 09:16:12 +0700

    add select option handler

--- src/components/StudentInput.js
+++ src/components/StudentInput.js
@@ -1,32 +1,59 @@
-import React from 'react'
-import '../styles/StudentInput.css'
-import {FaUserCircle} from 'react-icons/fa'
+import React from 'react';
+import '../styles/StudentInput.css';
+import {FaUserCircle} from 'react-icons/fa';
 
 const OPTIONS = [1,2,3,4,5,6,7,8,9,10]
 
-const Select = () => {
+const Select = ({
+    placeholder,
+    onSelect,
+    value
+}) => {
     return (
-        <div>
-            <select>
-                <option value=''>Pilih</option>
-                {
-                    OPTIONS.map((item) => {
-                        return <option value={item} key={item}>{item}</option>
-                    })
-                }
-            </select>
-        </div>
+        <select onChange={onSelect} value={value}>
+            <option value="">{placeholder}</option>
+            {
+                OPTIONS.map((item) => {
+                    return <option value={item} key={item}>{item}</option>
+                })
+            }
+        </select>
     )
 }
-export default function StudentInput() {
-  return (
-    <div className='aspectInput'>
+export default function StudentInput(props) {
+
+    const {data, aspects, studentIndex, onSelect} = props;
+    const handleSelect = (e, index) => {
+        const value = Number(e.target.value);
+        const temp = data;
+        temp[index] = value
+        
+        onSelect(temp);
+        console.log('clg temp child', value, temp, index)
+
+    }
+
+    // console.log(`clg data ${studentIndex}`, data);
+    
+    return (
+        <div className='aspectInput'>
         <div>
             <span><FaUserCircle/></span>
-            <span>Mahasiswa 1</span>
+            <span>Mahasiswa {studentIndex+1}</span>
         </div>
         <div>
-            <Select />
+            {
+                aspects.map((item, index) => {
+                    return(
+                        <Select 
+                            onSelect={(e) => handleSelect(e, index)}
+                            key={item}
+                            value={data[index]}
+                            placeholder='Pilih...'
+                        />
+                    )
+                })
+            }
         </div>
     </div>
   )
